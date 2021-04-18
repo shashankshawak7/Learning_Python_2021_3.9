0ee1f07b09b60000de5821314305d3495dc56fd8
shashank shawak <shashankshawak7@gmail.com>
2021-04-18 19:26:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+#python
+#python is a programming language
+
+question 1.  why python?
+                -> easy to learn
+                -> scripting+programming(procedural and non procedural)
+                -> a vast amount of libraries
+
+    #machine learning and AI
+        code is vastly written in python
+
+
+procedural code:
+            step wise code
+            1. open a file
+            # f= open('file')
+            2. read the file
+                read=f.read()
+            3. write the file
+                write=f.write()
+            4. close the file
+                f.close()
+non procedural code:
+                functions
+                class
+
+-->  prebuilt code for using in any code
+library for finding square
+def square(x):
+    return x*x
+import the library in the code
+then run the code
+
+import square
+square(5)
+
+
+#versions
+2020: you can write code but support is off
+    -> 2.1.0
+    -> 3.6.0
+
+2021:
+        -> 3.8
+
+        -> 3.9
+
+when ever a new release comes:
+        backward compatibility is not not available
+
+2.1 -> print "shashank"
+
+3.x -> print ("shashank")
+
+
+major version -> 2 and 3
+minor version -> .8 ,.1,.9
+#softwares are available that do this for you.
+
+
+
+
+
+
